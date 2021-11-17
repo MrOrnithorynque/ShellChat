6211ff415b60ce4fbb066e9fd65511a875709702
Louis <louis.obeniche-rousse@epitech.eu>
2021-11-17 22:25:17 +0100

    <rm>: MyLib

--- Libraries/MyLib/Includes/my.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-namespace my
-{
-    void print_err(std::string error_msg);
-}
--- Libraries/MyLib/String/print_err.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "my.hpp"
-
-void my::print_err(std::string error_msg)
-{
-    std::cerr << "\033[31m" << error_msg << "\033[0m" << "\n";
-}
