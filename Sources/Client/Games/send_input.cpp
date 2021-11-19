84b759b7331d8a3a16da95f13840b1d09400e66e
Louis Obéniche Rousse <Louis.Obeniche-Rousse@ganymedrobotics.com>
2021-11-19 10:25:22 +0100

    <add>: ncurse lib

--- Sources/Client/Games/send_input.cpp
+++ Sources/Client/Games/send_input.cpp
@@ -4,3 +4,16 @@
 **   Send player input.
 */
 
+#include <ncurses.h>
+
+#include "network.hpp"
+
+// FUNCTION is in a thread
+void send_input(void)
+{
+	initscr();
+  	keypad(stdscr, TRUE);
+	getch();
+
+	// send the input to server
+}
